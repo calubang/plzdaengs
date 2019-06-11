a91b62ae6605fa7a5fa241608cbecea6805d95c9
Administrator <claire97j@naver.com>
2019-06-11 17:59:46 +0900

    19/06/11 윤영 update > DB작업 계속해야함

--- plzdaengs/src/com/plzdaeng/diary/controller/DiaryServlet.java
+++ plzdaengs/src/com/plzdaeng/diary/controller/DiaryServlet.java
@@ -10,32 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class DiaryServlet
  */
-@WebServlet("/DiaryServlet")
+@WebServlet("/plzDiary")
 public class DiaryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DiaryServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("넘어오니?");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("넘어오니?2");
 	}
 
 }
