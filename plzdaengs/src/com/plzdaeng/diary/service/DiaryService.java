21e87fec804f19c55c3ce0eb8cafca1fbdfbfb3a
calubang <acalubang@gmail.com>
2019-06-12 23:14:55 +0900

    19.06.12 안병욱 업데이트
    다이어리 달마다 데이터 초기화 작업 추가

--- plzdaengs/src/com/plzdaeng/diary/service/DiaryService.java
+++ plzdaengs/src/com/plzdaeng/diary/service/DiaryService.java
@@ -1,11 +1,19 @@
 package com.plzdaeng.diary.service;
 
+import java.util.List;
+
 import com.plzdaeng.diary.dao.diaryDao;
+import com.plzdaeng.dto.DiaryDto;
+import com.plzdaeng.dto.UserDto;
 
 public class DiaryService {
 	private diaryDao dao;
 	
 	public DiaryService() {
 		dao = new diaryDao();
+	}
+
+	public List<DiaryDto> initDataByMonth(String date, UserDto user) {
+		return dao.selectAllByMonth(date, user);
 	}
 }
